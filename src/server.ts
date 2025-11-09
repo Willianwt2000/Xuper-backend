99fc295d4bdb858090bc5913424e1866ec1c622d
Willianwt2000 <willianmetelien@gmail.com>
2025-11-09 00:16:07 -0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("XUPER Backend is running 🚀");
+});
+
+
+
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  throw new Error("PORT is not defined in environment variables");
+}
+
+app.listen(PORT, () => {
+  console.log("==================================");
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
